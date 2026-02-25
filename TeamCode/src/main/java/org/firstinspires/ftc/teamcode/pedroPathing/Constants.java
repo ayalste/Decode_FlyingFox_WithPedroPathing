a82e1c2636a496a2ee545927538f67698337e55a
ayalste <ayalste@gmail.com>
2026-02-25 18:34:13 +0200

    tuning changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -39,8 +39,8 @@ public class Constants {
                             RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
                     )
             )
-            .forwardPodY(0)
-            .strafePodX(1.4)
+            .forwardPodY(6.3)
+            .strafePodX(7.28)
             .forwardEncoderDirection(Encoder.FORWARD)
             .strafeEncoderDirection(Encoder.REVERSE)
             .forwardTicksToInches(0.013253769971005715)
