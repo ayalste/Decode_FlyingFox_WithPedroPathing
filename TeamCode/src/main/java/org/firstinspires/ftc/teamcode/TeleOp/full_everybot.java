c03a2a31dd3bf3221e331d8d751bf4dd37cf9e05
Fedya9 <grofed4@gmail.com>
2026-02-25 16:57:51 +0200

    full olala

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/full_everybot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/full_everybot.java
@@ -6,74 +6,87 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
-@TeleOp(name = "full_everybot", group = "Teleop")
+@TeleOp(name = "full_TOTO:)", group = "Teleop")
 public class full_everybot extends OpMode {
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Declarations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //////////////////////////
+    //////// Battery ////////
+    //////////////////////////
     private VoltageSensor batteryVoltageSensor;
 
+    //////////////////////////
+    ///////// Timer //////////
+    //////////////////////////
     private ElapsedTime runtime = new ElapsedTime();
 
-    // Drive motors
-    private DcMotor leftFrontDrive;
-    private DcMotor leftBackDrive;
-    private DcMotor rightFrontDrive;
-    private DcMotor rightBackDrive;
+    //////////////////////////
+    ///// Driving system /////
+    //////////////////////////
+        private DcMotor leftFrontDrive;
+        private DcMotor leftBackDrive;
+        private DcMotor rightFrontDrive;
+        private DcMotor rightBackDrive;
 
-    // Catapult motors
-    private DcMotor catapult1;
-    private DcMotor catapult2;
+    //////////////////////////
+    //////// Launcher ////////
+    //////////////////////////
+        private DcMotor catapult1;
+        private DcMotor catapult2;
 
-    // Intake motor
-    private DcMotor intakeMotor;
+        private enum CatapultModes { UP, DOWN, HOLD }
+        private CatapultModes pivotMode;
 
-    // Foot motor
+        private boolean shaking = false;
+        private int shakeStep = 0;
+        private ElapsedTime shakeTimer = new ElapsedTime();
+
+    //////////////////////////
+    ///////// Intake /////////
+    //////////////////////////
+        private DcMotor intakeMotor;
+
+    //////////////////////////
+    //////// End game ////////
+    //////////////////////////
     private DcMotor foot;
 
-    // encoder foot
+    // Encoder
     static final int MOVE_TICKS = 2300;   // כמה שצריך להסתובב
     static final double MOVE_POWER = 1;
     boolean lastA = false;
     boolean lastB = false;
     boolean isActive = false;
 
-    private enum CatapultModes { UP, DOWN, HOLD }
-    private CatapultModes pivotMode;
+    // setting the foot to up (homing)
+    boolean homing = true;
+    int lastEncoderPosition = 0;
+    ElapsedTime stallTimer = new ElapsedTime();
+    static final double HOMING_POWER = 0.15;
+    static final int MOVEMENT_THRESHOLD = 5;
+    static final int STALL_TIME_MS = 300;
 
-    private enum FootModes { UP, DOWN, BRAKE }
-    private FootModes footMode;
-
-    private boolean shaking = false;
-    private int shakeStep = 0;
-    private ElapsedTime shakeTimer = new ElapsedTime();
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Actual Code ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     @Override
     public void init() {
+        //////////////////////////
+        /// Updating telemetry ///
+        //////////////////////////
         telemetry.addData("Status", "Initializing");
 
-        // Drive motors
+        //////////////////////////
+        //////// Battery /////////
+        //////////////////////////
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
+
+        //////////////////////////
+        ///// Driving system /////
+        //////////////////////////
         leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
         leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-
-        // Catapult motors
-        catapult1 = hardwareMap.get(DcMotor.class, "catapult_motor1");
-        catapult2 = hardwareMap.get(DcMotor.class, "catapult_motor2");
-
-        // Intake
-        intakeMotor = hardwareMap.get(DcMotor.class, "intake_Motor");
-
-        // Foot
-        foot = hardwareMap.get(DcMotor.class, "foot");
-        foot.setDirection(DcMotor.Direction.REVERSE);
-        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //encoder
-        foot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        foot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Drive directions
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -81,16 +94,37 @@ public class full_everybot extends OpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
+        //////////////////////////
+        //////// Launcher ////////
+        //////////////////////////
+        catapult1 = hardwareMap.get(DcMotor.class, "catapult_motor1");
+        catapult2 = hardwareMap.get(DcMotor.class, "catapult_motor2");
+
         // Catapult directions
         catapult1.setDirection(DcMotor.Direction.REVERSE);
         catapult2.setDirection(DcMotor.Direction.FORWARD);
 
+        //////////////////////////
+        ///////// Intake /////////
+        //////////////////////////
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake_Motor");
+
         intakeMotor.setDirection(DcMotor.Direction.FORWARD);
 
-        // Battery sensor
-        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
+        //////////////////////////
+        //////// End game ////////
+        //////////////////////////
+        foot = hardwareMap.get(DcMotor.class, "foot");
+        foot.setDirection(DcMotor.Direction.REVERSE);
+        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //encoder
+        foot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        foot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        telemetry.addData("Status", "Initialized");
+        //////////////////////////
+        /// Updating telemetry ///
+        //////////////////////////
+        telemetry.addData("Status", "Initialized & Ready ;)");
         telemetry.update();
     }
 
@@ -100,10 +134,10 @@ public class full_everybot extends OpMode {
 
 
         //setting the endgame
-        int newTarget = foot.getCurrentPosition() + MOVE_TICKS;
-        foot.setTargetPosition(newTarget);
-        foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        foot.setPower(0.1);
+        homing = true;
+        stallTimer.reset();
+        lastEncoderPosition = foot.getCurrentPosition();
+
     }
 
     @Override
@@ -121,62 +155,71 @@ public class full_everybot extends OpMode {
         leftBackDrive.setPower(speed - turn + rotation);
         rightBackDrive.setPower(speed + turn - rotation);
 
-        /* =========================
-           FOOT (gamepad1)
-        ========================= */
-//
-//        boolean footDownButton = gamepad1.a;
-//        boolean footUpButton = gamepad1.b;
-//
-//        if (footDownButton && footUpButton) {
-//            footDownButton = false;
-//        }
-//
-//        if (footDownButton) {
-//            footMode = FootModes.DOWN;
-//            foot.setPower(FOOT_DOWN_POWER);
-//        }
-//        else if (footUpButton) {
-//            footMode = FootModes.UP;
-//            foot.setPower(FOOT_UP_POWER);
-//        }
-//        else {
-//            footMode = FootModes.BRAKE;
-//            foot.setPower(FOOT_OFF_POWER);
-//        }
-
+        // =========================
+        //      FOOT (gamepad1)
+        // =========================
         boolean currentA = gamepad1.a;
         boolean currentB = gamepad1.b;
         boolean aPressed = currentA && !lastA;
         boolean bPressed = currentB && !lastB;
 
-        if (aPressed && !foot.isBusy() && !isActive) {
+        // HOMING LOGIC
+        if (homing) {
 
-            int newTarget = foot.getCurrentPosition() - MOVE_TICKS;
+            foot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            foot.setPower(HOMING_POWER);
 
-            foot.setTargetPosition(newTarget);
-            foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            foot.setPower(MOVE_POWER);
+            int currentPosition = foot.getCurrentPosition();
 
-            isActive = !isActive;
+            if (Math.abs(currentPosition - lastEncoderPosition) > MOVEMENT_THRESHOLD) {
+                stallTimer.reset();
+                lastEncoderPosition = currentPosition;
+            }
+
+            if (stallTimer.milliseconds() > STALL_TIME_MS) {
+
+                foot.setPower(0);
+
+                foot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                foot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                homing = false;
+            }
+
+            telemetry.addData("HOMING", "Running...");
+            telemetry.update();
+        }
+        else
+        {
+            if (aPressed && !foot.isBusy() && !isActive) {
+
+                int newTarget = foot.getCurrentPosition() - MOVE_TICKS;
+
+                foot.setTargetPosition(newTarget);
+                foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                foot.setPower(MOVE_POWER);
+
+                isActive = true;
+            }
+
+            if (bPressed && !foot.isBusy() && isActive) {
+
+                int newTarget = foot.getCurrentPosition() + MOVE_TICKS;
+
+                foot.setTargetPosition(newTarget);
+                foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                foot.setPower(MOVE_POWER);
+
+                isActive = false;
+            }
+
+            telemetry.addData("Position", foot.getCurrentPosition());
+            telemetry.update();
+
+            lastA = currentA;
+            lastB = currentB;
         }
 
-        if (bPressed && !foot.isBusy() && isActive) {
-
-            int newTarget = foot.getCurrentPosition() + MOVE_TICKS;
-
-            foot.setTargetPosition(newTarget);
-            foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            foot.setPower(MOVE_POWER);
-
-            isActive = !isActive;
-        }
-
-        telemetry.addData("Position", foot.getCurrentPosition());
-        telemetry.update();
-
-        lastA = currentA;
-        lastB = currentB;
 
 
         /* =========================
@@ -261,7 +304,7 @@ public class full_everybot extends OpMode {
         telemetry.addData("Run Time", runtime.toString());
         telemetry.addData("Catapult mode", pivotMode);
         telemetry.addData("Intake power", intakeMotor.getPower());
-        telemetry.addData("Foot mode", footMode);
+        telemetry.addData("Foot mode (down)", isActive);
 
         telemetry.addData("Battery Voltage", batteryVoltageSensor.getVoltage());
         telemetry.addData("Battery %", "%.1f", getBatteryPercent());
